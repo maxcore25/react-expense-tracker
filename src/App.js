27e03fd367a19c518339e2879bdd1849bb5c33c2
maxcore25 <mkorobov2002@mail.ru>
2022-05-20 22:40:29 +0300

    Header Imported

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
