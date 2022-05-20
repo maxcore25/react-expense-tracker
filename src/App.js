bb9e60e582ae0334e14eb6bd658b67ca681e9d0f
maxcore25 <mkorobov2002@mail.ru>
2022-05-20 23:14:34 +0300

    Balance Component Into App Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import Balance from './components/Balance';
 import Header from './components/Header';
 
 function App() {
   return (
     <div>
       <Header />
-      <div className='container'></div>
+      <div className='container'>
+        <Balance />
+      </div>
     </div>
   );
 }
