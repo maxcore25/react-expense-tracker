ca776a98fa91037cb16ed7255a86c7ec2c46c664
maxcore25 <mkorobov2002@mail.ru>
2022-05-20 23:15:58 +0300

    IncomeExpenses Component Into App Added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Balance from './components/Balance';
 import Header from './components/Header';
+import IncomeExpenses from './components/IncomeExpenses';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <div className='container'>
         <Balance />
+        <IncomeExpenses />
       </div>
     </div>
   );
