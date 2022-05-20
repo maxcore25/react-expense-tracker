4d3f23e6f7e0eb87136dd454f42863ab98b2f34b
maxcore25 <mkorobov2002@mail.ru>
2022-05-20 22:48:22 +0300

    Balance Component Created

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div>
       <Header />
+      <div className='container'></div>
     </div>
   );
 }
---
+++ src/components/Balance.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Balance() {
+  return <div>Balance</div>;
+}
