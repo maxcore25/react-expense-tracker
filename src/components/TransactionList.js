09387934305978b30debb7552aac56ec478970b5
maxcore25 <mkorobov2002@mail.ru>
2022-05-21 22:53:23 +0300

    Transaction Imported

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { GlobalContext } from '../context/GlobalState';
+import Transaction from './Transaction';
 
 export default function TransactionList() {
   const { transactions } = useContext(GlobalContext);
@@ -9,12 +10,8 @@ export default function TransactionList() {
       <h3>History</h3>
       <ul className='list'>
         {transactions.map(transaction => (
-          <li className='minus'>
-            Cash <span>-$400</span>
-            <button className='delete-btn'>x</button>
-          </li>
+          <Transaction key={transaction.id} transaction={transaction} />
         ))}
-        {/* {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))} */}
       </ul>
     </>
   );
