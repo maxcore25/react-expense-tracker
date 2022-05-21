fa96154feb7cfa185b35dc8d815650284a8c3a41
maxcore25 <mkorobov2002@mail.ru>
2022-05-21 22:08:30 +0300

    JSX In IncomeExpenses Written

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
 export default function IncomeExpenses() {
-  return <div>IncomeExpenses</div>;
+  return (
+    <div className='inc-exp-container'>
+      <div>
+        <h4>Income</h4>
+        <p className='money plus'>{999}</p>
+      </div>
+      <div>
+        <h4>Expense</h4>
+        <p className='money minus'>{999}</p>
+      </div>
+    </div>
+  );
 }
