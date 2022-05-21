05f5e0c5a328cd295f329174dd4bd2d5af227231
maxcore25 <mkorobov2002@mail.ru>
2022-05-21 22:15:06 +0300

    AddTransaction Into App Added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import AddTransaction from './components/AddTransaction';
 import Balance from './components/Balance';
 import Header from './components/Header';
 import IncomeExpenses from './components/IncomeExpenses';
@@ -12,6 +13,7 @@ function App() {
         <Balance />
         <IncomeExpenses />
         <TransactionList />
+        <AddTransaction />
       </div>
     </div>
   );
