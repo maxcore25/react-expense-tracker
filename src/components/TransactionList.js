8990861c02aeb4466281d25f0c8904640c694fce
maxcore25 <mkorobov2002@mail.ru>
2022-05-21 22:13:10 +0300

    TransactionList JSX Item Added

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -5,6 +5,10 @@ export default function TransactionList() {
     <>
       <h3>History</h3>
       <ul className='list'>
+        <li className='minus'>
+          Cash <span>-$400</span>
+          <button className='delete-btn'>x</button>
+        </li>
         {/* {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))} */}
       </ul>
     </>
