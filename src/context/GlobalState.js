d72ecd3746bce78b5e4331044d5eb3f2b8b318a0
maxcore25 <mkorobov2002@mail.ru>
2022-05-21 22:27:59 +0300

    Initial State Example Added

---
+++ src/context/GlobalState.js
@@ -0,0 +1,11 @@
+import React, { createContext, useReducer } from 'react';
+
+// Initial state
+const initialState = {
+  transactions: [
+    { id: 1, text: 'Flower', amount: -20 },
+    { id: 2, text: 'Salary', amount: 300 },
+    { id: 3, text: 'Book', amount: -10 },
+    { id: 4, text: 'Camera', amount: 150 },
+  ],
+};
