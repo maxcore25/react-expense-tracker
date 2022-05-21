3385dfd5cafdff58b4c5c874ed10d66aa987b7f8
maxcore25 <mkorobov2002@mail.ru>
2022-05-21 22:16:53 +0300

    AddTransaction JSX Written

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,5 +1,33 @@
 import React from 'react';
 
 export default function AddTransaction() {
-  return <div>AddTransaction</div>;
+  return (
+    <>
+      <h3>Add new transaction</h3>
+      <form onSubmit={() => console.log()}>
+        <div className='form-control'>
+          <label htmlFor='text'>Text</label>
+          <input
+            type='text'
+            value={'text'}
+            onChange={e => console.log(e.target.value)}
+            placeholder='Enter text...'
+          />
+        </div>
+        <div className='form-control'>
+          <label htmlFor='amount'>
+            Amount <br />
+            (negative - expense, positive - income)
+          </label>
+          <input
+            type='number'
+            value={123}
+            onChange={e => console.log(e.target.value)}
+            placeholder='Enter amount...'
+          />
+        </div>
+        <button className='btn'>Add transaction</button>
+      </form>
+    </>
+  );
 }
