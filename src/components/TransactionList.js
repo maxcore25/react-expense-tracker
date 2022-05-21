0cff2be50210510906d25122b84d0763349377e6
maxcore25 <mkorobov2002@mail.ru>
2022-05-21 22:50:05 +0300

    Transactions Mapping Written

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -2,16 +2,18 @@ import React, { useContext } from 'react';
 import { GlobalContext } from '../context/GlobalState';
 
 export default function TransactionList() {
-  const context = useContext(GlobalContext);
+  const { transactions } = useContext(GlobalContext);
 
   return (
     <>
       <h3>History</h3>
       <ul className='list'>
-        <li className='minus'>
-          Cash <span>-$400</span>
-          <button className='delete-btn'>x</button>
-        </li>
+        {transactions.map(transaction => (
+          <li className='minus'>
+            Cash <span>-$400</span>
+            <button className='delete-btn'>x</button>
+          </li>
+        ))}
         {/* {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))} */}
       </ul>
     </>
