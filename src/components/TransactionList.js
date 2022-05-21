cd34b92cc98edfdbb35ca6c764c0d61ca71a6162
maxcore25 <mkorobov2002@mail.ru>
2022-05-21 22:47:00 +0300

    GlobalContext In TransactionList Used

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 export default function TransactionList() {
+  const context = useContext(GlobalContext);
+
   return (
     <>
       <h3>History</h3>
