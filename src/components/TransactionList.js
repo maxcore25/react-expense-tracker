1644e6a2b272124ec93011d5274507ce92337c8e
maxcore25 <mkorobov2002@mail.ru>
2022-05-21 22:11:38 +0300

    TransactionList Basic JSX Added

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
 export default function TransactionList() {
-  return <div>TransactionList</div>;
+  return (
+    <>
+      <h3>History</h3>
+      <ul className='list'>
+        {/* {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))} */}
+      </ul>
+    </>
+  );
 }
