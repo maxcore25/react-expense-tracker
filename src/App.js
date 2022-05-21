10baa99e30644c813dc2f41f1335c23cdb1c44fa
maxcore25 <mkorobov2002@mail.ru>
2022-05-21 22:10:31 +0300

    TransactionList Into App Added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Balance from './components/Balance';
 import Header from './components/Header';
 import IncomeExpenses from './components/IncomeExpenses';
+import TransactionList from './components/TransactionList';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <div className='container'>
         <Balance />
         <IncomeExpenses />
+        <TransactionList />
       </div>
     </div>
   );
