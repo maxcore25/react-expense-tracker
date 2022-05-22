1275cfbb7175f199e0c36032371c1d943c4e3fee
maxcore25 <mkorobov2002@mail.ru>
2022-05-22 23:48:51 +0300

    Count Expense And Income Variables Added

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -4,6 +4,18 @@ import { GlobalContext } from '../context/GlobalState';
 export default function IncomeExpenses() {
   const { transactions } = useContext(GlobalContext);
 
+  const amounts = transactions.map(transaction => transaction.amount);
+
+  const income = amounts
+    .filter(item => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
+  const expense = (
+    amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -1
+  ).toFixed(2);
+
   return (
     <div className='inc-exp-container'>
       <div>
