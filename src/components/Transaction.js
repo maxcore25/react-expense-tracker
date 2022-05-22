ab673dbf1a70ff0a6a245f0c45fb89f5c9959e28
maxcore25 <mkorobov2002@mail.ru>
2022-05-22 23:37:27 +0300

    Dynamic Class Added

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -4,7 +4,7 @@ export default function Transaction({ transaction }) {
   const sign = transaction.amount < 0 ? '-' : '+';
 
   return (
-    <li className='minus'>
+    <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
       {transaction.text}{' '}
       <span>
         {sign}${Math.abs(transaction.amount)}
